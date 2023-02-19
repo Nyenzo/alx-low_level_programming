e72bb625e10708678b6bdfb56d561e7f6027e242
Nyenzo <nyenzoisabwa@gmail.com>
2023-02-20 01:33:49 +0300

    C

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,28 +2,23 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Program Entry point
+ *main - Entry point
  *
- * Description: A program to determine wether an integer is -ve, +ve or 0
- *
- * Return: 0 indicates success
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int n, last;
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 
-	last = n % 10;
-
-	if (last >= 6)
+	if ((n % 10) == 0)
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	else if ((n % 10) > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
-	else if (last < 6 && last != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	else
-		printf("Last digit of %d is 0 and is 0\n", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+
 	return (0);
 }
