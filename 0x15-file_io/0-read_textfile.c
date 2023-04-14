dc53935d54ba14f4d01aac3b6fc9194c9cc01e12
Nyenzo <nyenzoisabwa@gmail.com>
2023-04-14 19:40:17 +0300

    File IO

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,38 @@
-/* File: file_operations.c
- * Author: Nyenzo
- * Description: Implementation of file operations functions
- */
-
 #include "main.h"
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+/**
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ * Return: the number of letters printed, or 0 if it failed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	int i, y;
+	char *buf;
 
-/* Function to read and print a text file */
-ssize_t read_textfile(const char *filename, size_t letters) {
-    if (filename == NULL) {
-        return 0; /* Return 0 if filename is NULL */
-    }
-
-    int fd = open(filename, O_RDONLY); /* Open file in read-only mode */
-    if (fd == -1) {
-        perror("Error opening file");
-        return 0; /* Return 0 on error */
-    }
-
-    char *buffer = (char *)malloc(sizeof(char) * (letters + 1)); /* Allocate memory for buffer */
-    if (buffer == NULL) {
-        perror("Error allocating memory");
-        close(fd);
-        return 0; /* Return 0 on error */
-    }
-
-    ssize_t bytes_read = read(fd, buffer, letters); /* Read from file into buffer */
-    if (bytes_read == -1) {
-        perror("Error reading file");
-        close(fd);
-        free(buffer);
-        return 0; /* Return 0 on error */
-    }
-
-    buffer[bytes_read] = '\0'; /* Null-terminate the buffer */
-    printf("%s", buffer); /* Print buffer contents */
-
-    close(fd);
-    free(buffer);
-    return bytes_read; /* Return number of bytes read */
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+	i = read(fd, buf, letters);
+	if (i < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	buf[i] = '\0';
+	close(fd);
+	y = write(STDOUT_FILENO, buf, i);
+	if (y < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	return (y);
 }
