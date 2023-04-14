7540edf49a9e29e163a8d629d84362676278eeff
Nyenzo <nyenzoisabwa@gmail.com>
2023-04-14 19:41:19 +0300

    File IO

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,21 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/************
- * LIBRARIES
- *************/
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-/*****************
- * Prototypes*
- *****************/
+/* Function to read and print a text file */
 ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
+/* Function to append text to a file */
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* MAIN.H */
+#endif /* MAIN_H */
