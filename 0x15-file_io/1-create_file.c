aa45f8a3491fb981af803a1d4721fed0c7919a8b
Nyenzo <nyenzoisabwa@gmail.com>
2023-04-14 19:55:33 +0300

    File IO

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,4 @@
-/* File: file to be read
+/* File: file_operations.c
  * Author: Nyenzo
  * Description: Implementation of file operations functions
  */
@@ -12,15 +12,15 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-/* Function to append text to a file */
-int append_text_to_file(const char *filename, char *text_content) {
+/* Function to create a file */
+int create_file(const char *filename, char *text_content) {
     if (filename == NULL) {
         return -1; /* Return -1 if filename is NULL */
     }
 
-    int fd = open(filename, O_WRONLY | O_APPEND); /* Open file in append mode */
+    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR); /* Open file with desired permissions */
     if (fd == -1) {
-        perror("Error opening file");
+        perror("Error creating file");
         return -1; /* Return -1 on error */
     }
 
