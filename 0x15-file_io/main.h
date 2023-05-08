4415768b4116ed5137dda48e8341aebb43096786
Nyenzo <nyenzoisabwa@gmail.com>
2023-05-08 23:52:00 +0300

    file IO

--- 0x15-file_io/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <unistd.h>
-
-/* Function to read and print a text file */
-ssize_t read_textfile(const char *filename, size_t letters);
-
-#endif /* MAIN_H */
