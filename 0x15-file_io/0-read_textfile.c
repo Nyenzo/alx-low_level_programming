434be8aac05eb238655308cddeed1a9d04432db3
Nyenzo <nyenzoisabwa@gmail.com>
2023-05-08 23:43:56 +0300

    file IO

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,21 +1,50 @@
-/* File: main.c
- * Author: Nyenzo
- * Description: Example usage of file operations functions
- */
-
 #include "main.h"
-#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
-int main() {
-    const char *filename = "example.txt";
-    size_t letters = 100;
+/**
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ *         Otherwise, the actual number of bytes the function can read and print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	if (filename == NULL)
+		return (0);
 
-    ssize_t result = read_textfile(filename, letters);
-    if (result == -1) {
-        printf("Failed to read file.\n");
-    } else {
-        printf("Read %zd letters from file.\n", result);
-    }
+	ssize_t fd, bytes_read, bytes_written;
+	char *buffer;
 
-    return 0;
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1) {
+		free(buffer);
+		return (0);
+	}
+
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1) {
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read) {
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+
+	return (bytes_written);
 }
