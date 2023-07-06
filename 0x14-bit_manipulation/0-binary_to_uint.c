fd6f2a2690b80f9d485b4550008fac84b1105b12
Nyenzo <nyenzoisabwa@gmail.com>
2023-07-06 06:59:34 +0300

    bit manipulation in C

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,29 +1,31 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
-unsigned int binary_to_uint(const char *b) {
-    if (b == NULL) {
-        printf("Error: Input string is NULL.\n");
-        return 0;
-    }
+/**
+ * binary_to_uint - Convert a binary number to an unsigned int
+ * @b: Char string representing the binary number
+ *
+ * Return: Converted decimal number or 0 if there is an unconvertible char
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int total, power;
+	int len;
 
-    unsigned int result = 0;
-    int i = 0;
+	if (b == NULL)
+		return (0);
 
-    // Loop through each character in the string until null terminator is reached
-    while (b[i] != '\0') {
-        // Check if the character is not '0' or '1'
-        if (b[i] != '0' && b[i] != '1') {
-            printf("Error: Invalid character '%c' at position %d.\n", b[i], i);
-            return 0;
-        }
+	for (len = 0; b[len]; len++)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+	}
 
-        // Shift the current result to the left by 1 bit
-        result <<= 1;
-        // Add the current character to the result after converting it from char to int
-        result += (b[i] - '0');
-        i++;
-    }
+	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
+	{
+		if (b[len] == '1')
+			total += power;
+	}
 
-    return result;
+	return (total);
 }
